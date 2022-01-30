d73586292e9733fe5b6c60e1c62d347f42f8fcd3
P Resnik <psresnik@gmail.com>
2022-01-30 17:20:28 -0500

    Debugged, fixed/simplified use of use_scores, other revisions

--- detection/coding_text_with_categories.py
+++ detection/coding_text_with_categories.py
@@ -13,13 +13,22 @@ import sys
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--infile', default=None)
-    parser.add_argument('--codedfile', default=None)
-    parser.add_argument('--clusterfile', default=None)
-    parser.add_argument('--outfile', default=None)
-    parser.add_argument('--primary_only', default=False, action='store_true')
-    parser.add_argument('--include_coder', default=None)
-    parser.add_argument('--use_scores', default=False, action='store_true')
+    parser.add_argument('--infile', default=None,
+                            help='Input text to process, one raw text unit per line')
+    parser.add_argument('--codedfile', default=None,
+                            help='CSV file: clusters coded into SCS categories')
+    parser.add_argument('--clusterfile', default=None,
+                            help='CSV file: cluster keywords and variants')
+    parser.add_argument('--outfile', default=None,
+                            help='Output one line per input line')
+    parser.add_argument('--primary_only', default=False, action='store_true',
+                            help='Use only primary SCS category codings, not secondary')
+    parser.add_argument('--include_coder', default=None,
+                            help='Coders to include, defaults to all of them')
+    parser.add_argument('--use_scores', default=False, action='store_true',
+                            help='Use cluster scores rather than counts in output')
+    parser.add_argument('--include_unigrams', default=None, action='store_true',
+                            help='Allow text/keyword matching with unigrams, defaults to false')
     args = parser.parse_args()
 
     if (args.infile is None or args.codedfile is None or args.clusterfile is None or args.outfile is None):
@@ -39,9 +48,10 @@ def check_coder(include_coder):
             print(f'Warning: the input name is not a valid coder. Please check again.')
 
 
-def read_doc(file, nlp):
+def read_doc(file, nlp, include_unigrams=False):
     # Read the document sample, use SpaCy to process the document
     result = []
+    raw_lines = []
     with open(file, 'r', encoding='utf8') as f:
         lines = f.readlines()
         for line in tqdm(lines, desc='Read doc'):
@@ -52,16 +62,20 @@ def read_doc(file, nlp):
 
             temp = []
             for word in nlp(line):
-                temp.append(str(word))
-            for item in ngrams(temp, 1):
-                line_ngram.append(' '.join(item))
+                # Lowercase
+                temp.append(str(word).lower()) 
+            if (include_unigrams):
+                # Unigrams not included by default since they tend to overgenerate
+                for item in ngrams(temp, 1):
+                    line_ngram.append(' '.join(item))
             for item in ngrams(temp, 2):
                 line_ngram.append(' '.join(item))
             for item in ngrams(temp, 3):
                 line_ngram.append(' '.join(item))
             result.append(line_ngram)
+            raw_lines.append(line)
 
-    return result
+    return result, raw_lines
 
 
 def read_code_and_cluster(code, cluster):
@@ -136,69 +150,64 @@ def filter_code(code, primary_only, include_coder=None):
     return code
 
 
-def process(args, lines, cluster, code, output_file='result.txt'):
+def process(args, lines, raw_lines, cluster, code, output_file='result.txt'):
     outf = open(output_file, 'w', encoding='utf8')
-    for line in tqdm(lines, desc='Process doc'):
+    for line, raw_line in tqdm(zip(lines,raw_lines), desc='Process doc', total=len(lines)):
         # Reset for a new line of text
         matched_clusters= []
         score = 0
         dic = defaultdict(int)
 
-        # If we choose to use score
-        if args.use_scores:
-            # Notice here we iterate the code table instead of the cluster table
-            # This is just to keep consistency when we match code with labels
-            for idx, _ in enumerate(list(code['Keyword'])):
-                clu = list(code['Cluster'])[idx]
-                variants = get_variants(cluster, clu)
-                
-                # This is just to be safe, in case there are duplicate clusters in the data
-                if clu in matched_clusters:
-                    continue
-                
-                # Iterate through all the variants, the keyword itself is included in it.
-                for var in variants: 
-                    if var not in line:
-                        continue 
+        print("\n-----\n")
+        print("DEBUG: Processing line: {}".format(raw_line))
+        
+        # Notice here we iterate the code table instead of the cluster table
+        # This is just to keep consistency when we match code with labels        
+        for idx, _ in enumerate(list(code['Keyword'])):
+            clu = list(code['Cluster'])[idx]
+            variants = get_variants(cluster, clu)
+            # print(" DEBUG: Considering idx={}, clu={}, variants={}".format(idx,clu,variants))
 
-                    score += float(list(cluster['Score'])[idx])
-                    matched_clusters.append(clu)
-                    # if one variant has appeared in the text, the cluster is already matched
-                    # There is no need to iterate through this cluster anymore
-                    break
-
-            # Write the scores. If there is no match in the line, the score will be 0
-            outf.write(f'{score}\n')
-
-        # If we choose to use the count instead of score
-        else:
-            for idx, _ in enumerate(list(code['Keyword'])):
-                clu = list(code['Cluster'])[idx]
-                variants = get_variants(cluster, clu)
-                
-                if clu in matched_clusters:
-                    continue
-                
-                for var in variants:
-                    if var not in line:
-                        continue
-
-                    # Iterate through the valid columns, and add the labels to the dictionary
-                    for column in code.columns.to_list()[2:]:
-                        if not pd.isna(list(code[column])[idx]) and \
-                                    str(list(code[column])[idx]) != 'None of the Above':
-                            dic[list(code[column])[idx]] += 1
-                    matched_clusters.append(clu)
-
-                    break
+            # This is just to be safe, in case there are duplicate clusters in the data
+            if clu in matched_clusters:
+                continue
             
+            # Iterate through all the variants, the keyword itself is included in it.
+            for var in variants:
+                if var not in line:
+                    continue
+
+                # Iterate through the valid columns in the coders' assigment of primary/secondary codes,
+                # and add the labels to the dictionary
+                for column in code.columns.to_list()[2:]:
+                    if not pd.isna(list(code[column])[idx]) and \
+                                str(list(code[column])[idx]) != 'None of the Above':
+                        if args.use_scores:
+                            dic[list(code[column])[idx]] += float(list(cluster['Score'])[idx])
+                        else:
+                            dic[list(code[column])[idx]] += 1
+                        print(" DEBUG: variant '{}' matched {} (score {}) --> {}".format(
+                            var,
+                            clu,
+                            round(float(list(cluster['Score'])[idx]),2),
+                            list(code[column])[idx]))
+                        break # If it matched any coder, no need to consider other coders
+                matched_clusters.append(clu)
+
+                break
+
+            # Put together results with match counts (or scores)
             result = []
             if dic:
-                for k, v in dic.items():
+                sorted_dic = sorted(dic.items(), key = lambda kv: kv[1], reverse=True) # sort descending by count/score
+                for k, v in sorted_dic:
                     result.append(f'{k}:{v}')
-            # Write the counts per category. If there is no match in the line, there will be blank line 
-            outf.write(f'{", ".join(result)}\n')
+                    
+        # Write the counts/scores per category. If there is no match in the line, there will be blank line 
+        outf.write(f'{", ".join(result)}\n')
 
+        print("DEBUG: Result = {}".format(result))
+            
     outf.close()
 
 
@@ -216,7 +225,7 @@ if __name__ == '__main__':
     # Set up and read in the files. 
     # The coded_clusters_file is named "code" and the cluster_info_file is named "cluster" for simplicity
     nlp = spacy.load('en_core_web_sm')
-    lines = read_doc(args.infile, nlp)
+    lines, raw_lines = read_doc(args.infile, nlp, args.include_unigrams)
     code, cluster = read_code_and_cluster(args.codedfile, args.clusterfile)
 
     # Filter the code table based on the options
@@ -224,4 +233,4 @@ if __name__ == '__main__':
         code = filter_code(code, args.primary_only, args.include_coder)
 
     # Process the document and output to a text file named result 
-    process(args, lines, cluster, code, args.outfile)
+    process(args, lines, raw_lines, cluster, code, args.outfile)
